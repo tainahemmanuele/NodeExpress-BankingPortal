c9f1bac83d39889855983bc4d1c321c2f4655aa8
Tainah Emmanuele <tainahemmanuele@gmail.com>
2019-10-29 10:08:59 -0300

    Completed module

--- src/app.js
+++ src/app.js
@@ -4,8 +4,8 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(path.join(__dirname, '/public/')));
-app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
-app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/css', express.static(path.join(__dirname, '/public/css')));
+app.use('/js', express.static(path.join(__dirname, '/public/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
