f56b899f10acb33b974fbdcd64443f0dcbbbaf2e
Tainah Emmanuele <tainahemmanuele@gmail.com>
2019-10-29 09:58:37 -0300

    Primeira parte do projeto

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.use(express.static(path.join(__dirname, '/public/')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index',
+    {title: 'Index' })
+});
+
+app.listen(3000, () => {
+    console.log('PS Project Running on port 3000!');
+});
